0fb6c17e6eec8e7e9c7f9e2b3901a05634373e1b
Joshua LaGorce <joshlagorce@gmail.com>
2019-09-14 11:04:01 -0600

    updated readme

--- Bamazon/bamazonCustomer.js
+++ Bamazon/bamazonCustomer.js
@@ -10,59 +10,57 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-    
-      
-      inquirer.prompt([
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
 
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+
+    inquirer
+      .prompt([
         {
           type: "input",
           name: "product",
           message: "What is the ID for the product you want to purchase?"
         },
-      
+
         {
           type: "input",
           name: "quantity",
           message: "How many would you like to purchase?"
-        },
-      
-        
-      
-      ]).then(function(answer) {
-        var result = res.filter(function(row){
-          return row.id == answer.product
-      })[0];
-        var stk_value = parseInt(answer.quantity) - result.stock_quantity;
-        if (stk_value < 0){
-        connection.query(
-          "UPDATE products set ? where ? ",
-          [{
-            
-            stock_quantity: stk_value
-          },
-          {
-            id: answer.product,
-          }]
-          
-          
-      );
-        }else {
+        }
+      ])
+      .then(function(answer) {
+        var result = res.filter(function(row) {
+          return row.id == answer.product;
+        })[0];
+
+        console.log(result);
+        console.log(answer);
+        var stk_value = result.stock_quantity - parseInt(answer.quantity);
+
+        console.log(stk_value);
+
+        if (stk_value > 0) {
+          connection.query("UPDATE products set ? where ? ", [
+            {
+              stock_quantity: stk_value
+            },
+            {
+              id: answer.product
+            }
+          ]);
+          console.log("Thank you for your purchase!");
+        } else {
           console.log("Not enough in stock");
           afterConnection();
         }
-      
-        
+
         afterConnection();
-      
-    })
-    })
-  }
+      });
+  });
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bamazon-node-mySQL
+
+Bamazon app is created to run a back end data-base website relative to "Amazon" where a user can purchase products and the manager can add a new product, restock the inventory, check to see which items have a low inventory to make sure they dont run out. 
+
+The app is useful because it allows  you to update, delete, add and view your database quickly and easily.
+
+The user can get started by typing "node bamazonCustomer.js" and then select the "ID" of the item youd like to purchase and the quantity.
+
+The manager can get started by typing "node bamazonManager.js" and then select between "viewing items", "Adding an item", "Seeing items with low inventory (lower than 5)" and "updating inventory numbers."
+
